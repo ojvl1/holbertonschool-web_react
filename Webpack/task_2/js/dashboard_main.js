e9cd94c8e32f3009579be5729d71a3956196421e
ojvl1 <carlosantompietri175@gmail.com>
2024-12-09 15:59:13 -0400

    Adding CSS & Images

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -1,24 +1,35 @@
 import $ from "jquery";
 import _ from "lodash";
+import "../css/main.css";
 
+// Initialize count
 let count = 0;
 
+// Function to update counter
 function updateCounter() {
   count++;
   $("#count").text(`${count} clicks on the button`);
 }
 
+// Document ready function
 $(document).ready(function () {
+  // Create logo div
+  $("body").prepend('<div id="logo"></div>');
+
+  // Create and append paragraphs
   $("body").append("<p>Holberton Dashboard</p>");
   $("body").append("<p>Dashboard data for the students</p>");
 
+  // Create button with debounced click handler
   const debouncedUpdateCounter = _.debounce(updateCounter, 500);
   $("body").append(
     '<button id="clickButton">Click here to get started</button>'
   );
   $("#clickButton").on("click", debouncedUpdateCounter);
 
+  // Create count paragraph next to button
   $("body").append('<p id="count"></p>');
 
+  // Copyright paragraph
   $("body").append("<p>Copyright - Holberton School</p>");
 });
