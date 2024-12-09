3c7e7d25db4545ad6d70c7ce91440fc4c4210c9f
ojvl1 <carlosantompietri175@gmail.com>
2024-12-09 15:59:56 -0400

    webpack

--- Webpack/task_2/webpack.config.js
+++ Webpack/task_2/webpack.config.js
@@ -7,4 +7,29 @@ module.exports = {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+        parser: {
+          dataUrlCondition: {
+            maxSize: 8 * 1024, // 8kb
+          },
+        },
+      },
+    ],
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 };
