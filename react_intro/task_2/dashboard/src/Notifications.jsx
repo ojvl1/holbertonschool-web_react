71ce49b90aac9f595a8806ef34416b247dbae099
ojvl1 <antompietriosvaldo@gmail.com>
2024-12-23 01:18:50 -0400

    Update the Notifications

---
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+import { closeButton } from './assets/close-icon.png'
+
+function Notifications() {
+  return (
+    <div className='notifications'>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li>dangerouslySetInnerHTML={{ __html: getLatestNotification() }}</li>
+      </ul>
+      <button
+      style={{
+        position: 'absolute',
+        top: '5px',
+        right: '5px',
+        background: 'none',
+        border: 'none',
+        cursor: 'pointer',
+      }}
+      aria-label='Close'
+      onClick={() => console.log("Close button has been clicked")}>
+      <img src={closeButton} alt="close button" />
+      </button>
+    </div>
+  );
+}
+
+export default Notifications
