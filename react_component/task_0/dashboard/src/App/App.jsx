8cf5036450bc73b30cebf7ea0d77e778f455fb6d
ojvl1 <antompietriosvaldo@gmail.com>
2025-01-17 15:06:47 -0400

    fix

--- react_component/task_0/dashboard/src/App/App.jsx
+++ react_component/task_0/dashboard/src/App/App.jsx
@@ -5,40 +5,34 @@ import Login from '../Login/Login.jsx';
 import Footer from '../Footer/Footer.jsx';
 import Notifications from '../Notifications/Notifications.jsx';
 import CourseList from '../CourseList/CourseList.jsx';
+import PropTypes from 'prop-types';
 
-class App extends Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-    };
+const notificationsList = [
+  {
+    id: Math.random(),
+    type: 'default',
+    value: 'New course available'
+  },
+  {
+    id: Math.random(),
+    type: 'urgent',
+    value: 'New resume available'
+  },
+  {
+    id: Math.random(),
+    type: 'urgent',
+    value: '',
+    html: { __html: '<strong>Urgent requirement</strong>' }
   }
+];
+
+const coursesList = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+class App extends React.Component{
   render() {
-    const notificationsList = [
-      {
-        id: Math.random(),
-        type: 'default',
-        value: 'New course available'
-      },
-      {
-        id: Math.random(),
-        type: 'urgent',
-        value: 'New resume available'
-      },
-      {
-        id: Math.random(),
-        type: 'urgent',
-        value: '',
-        html: { __html: '<strong>Urgent requirement</strong>' }
-      }
-    ];
-
-    const coursesList = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
-    ];
-
     return (
       <>
         <Notifications notifications={notificationsList} displayDrawer={true} />
@@ -51,8 +45,16 @@ class App extends Component{
         <Footer />
       </>
     );
-  }}
-  
+  }
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+App.propTupes = {
+  isLoggedIn: PropTypes.bool,
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
