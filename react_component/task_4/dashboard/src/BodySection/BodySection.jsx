1abc62c7e9ce89025ddcf0f4eab0a931deb77657
ojvl1 <antompietriosvaldo@gmail.com>
2025-01-26 20:36:09 -0400

    fix

---
+++ react_component/task_4/dashboard/src/BodySection/BodySection.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const BodySection = ({ title, children }) => {
+  return(
+    <div className="bodySection">
+      <h2>{title}</h2>
+      {children}
+    </div>
+  )
+};
+
+BodySection.PropTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node,
+};
+
+BodySection.defaultProps = {
+  children: null,
+};
+
+export default BodySection;
