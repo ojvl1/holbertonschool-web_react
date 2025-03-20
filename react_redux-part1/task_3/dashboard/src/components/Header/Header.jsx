e058e2af5d6974502de62718bd054fe04a789b88
ojvl1 <antompietriosvaldo@gmail.com>
2025-03-20 15:56:47 -0400

    .

--- react_redux-part1/task_3/dashboard/src/components/Header/Header.jsx
+++ react_redux-part1/task_3/dashboard/src/components/Header/Header.jsx
@@ -1,5 +1,3 @@
-// task_3/dashboard/src/components/Header/Header.jsx
-
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../features/auth/authSlice';
